deefcea82a2885c70def461bc097601545686410
16gs24 <gage.sonntag@gmail.com>
2017-08-22 13:24:46 -0400

    Commented tutorial code

--- AdvancedApp.R
+++ AdvancedApp.R
@@ -1,29 +1,36 @@
+#load libraries
 library(shiny)
 library(tidyverse)
 
+#begin by building out UI. We will use a sidebar for selections and 
+# a main bar that has a scatterplot, summary stats and the head
 ui <- shinyUI(
+  #lets make dynamic pages
   fluidPage(
+    #build out our side panel
     sidebarLayout(
       sidebarPanel(
-        #X column
+        #Input for the x variable of scatterplot
         selectInput('xcol', 'X Variable', names(basic)),
-        #Color column
+        #Input for the color of the scatterplot
         selectInput('color','Color By',names(basic)),
-        #month slider
+        #Slider for a selection of months
         sliderInput(inputId='month',
                     label="Choose Months to View",
                     value=c(4,10),min=3,max=11),
-        #checkbox for day of week
+        #checkbox for what day of the week we want to view
         checkboxGroupInput(
           inputId='dayofweek',
           label='Day of Week',
           choices = unique(basic$Weekday),
           selected = unique(basic$Weekday))
       ) ,
+      #enough inputs. what outputs do we want to look at.
       mainPanel(
                 plotOutput('scatter'),
         #Show Summary stats
         verbatimTextOutput('summary'),
+        #show the head of our data
         tableOutput('raw')
       )
     )
@@ -33,25 +40,34 @@ ui <- shinyUI(
 # Server Code
 server <-   shinyServer(
   function(input, output) {
-    
+    #begin by reactively filtering the data
+    #filter for day of the week from selection, and month from slider
     filtered <- reactive({
       basic %>%
         filter(
             Weekday %in% input$dayofweek &
             Month %in% input$month)
       })
-      
+    #first output is our summary statistics  
     output$summary <- renderPrint({
-      data <- filtered()
+      #to get our data reactively filtered, we need to redefine our 
+      #data source inside render*()
+      data <- filtered() 
       summary(data$Attendance)
     })
+    
+    #next we will get a table of the head of 8 games
     output$raw <- renderTable({
       data <- filtered()
       head(data,8)
     })
+    
+    #finally build a ggplot with scatter plot of Attendance vs our choice
     output$scatter <- renderPlot({
       data <- filtered()
-      ggplot(data=data,aes_string(x=input$xcol,
+      
+      #we need aes_string since input$xcol isn't inside data
+      ggplot(data=data,aes_string(x=input$xcol, 
                                    y='Attendance',
                                    col=input$color))+
         geom_point()
